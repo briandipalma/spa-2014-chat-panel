1e006c10983ebd88a4f0985379ebad5de4ac5ed2
Brian Di Palma <offler@gmail.com>
2014-06-01 22:30:38 +0100

    Add ChatPanelStore.

---
+++ src/ChatPanelStore.js
@@ -0,0 +1,34 @@
+import {Store} from 'flux-es6';
+
+import ChatPanelConstants from './ChatPanelConstants';
+
+var messages = new Map();
+var messagesFromSelectedConversation = [];
+
+export default class extends Store {
+    getState() {
+        return messagesFromSelectedConversation;
+    }
+
+    handleDispatcherAction(payload) {
+		var action = payload.action;
+
+		switch (action.actionType) {
+            case ChatPanelConstants.MESSAGE_RECEIVED:
+				messages.get(action.userName).push(action.messageData);
+                break;
+            case ChatPanelConstants.MESSAGES_RECEIVED:
+                messages = action.messages;
+                break;
+            case ChatPanelConstants.RECENT_CONVERSATION_SELECTED:
+				messagesFromSelectedConversation = messages.get(action.userName);
+                break;
+            default:
+                return true;
+        }
+
+        this.emitChange();
+
+        return true;
+    }
+}
