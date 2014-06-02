c3dc69f71d4e42facfea6db79244124b8dc9ca2a
Brian Di Palma <offler@gmail.com>
2014-06-02 10:28:45 +0100

    Export the ChatPanelElement from the package main.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-export function printHello() {
-	console.info("Hello");
+import {Dispatcher} from "flux-es6";
+
+import ChatPanelStore from "./ChatPanelStore";
+import ChatPanelActions from "./ChatPanelActions";
+
+export function createStoreAndActions() {
+    var chatPanelDispatcher = new Dispatcher();
+    var chatPanelStore = new ChatPanelStore();
+    var chatPanelActions = new ChatPanelActions(chatPanelDispatcher);
+
+    chatPanelDispatcher.register((payload) => chatPanelStore.handleDispatcherAction(payload));
+
+    return [chatPanelStore, chatPanelActions];
 }
+
+export {ChatPanelElement} from "./ChatPanelElement";
